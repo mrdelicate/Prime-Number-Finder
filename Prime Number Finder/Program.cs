3bce9e8ed04195f08cddf10018326bd64be87c57
Eric Johnson <eric.johnson@revention.com>
2023-02-28 12:28:45 -0600

    Add project files.

---
+++ Prime Number Finder/Program.cs
@@ -0,0 +1,24 @@
+﻿while (true)
+{
+    int maximumNumber, currentNumber = 0, flag = 0, primeCounter = 0;
+    Console.Write("Find prime numbers up to what number? ");
+    maximumNumber = int.Parse(Console.ReadLine());
+    for (currentNumber = 2; currentNumber <= maximumNumber; currentNumber++)
+    {
+        for (var i = 2; i < currentNumber; i++)
+        {
+            if (currentNumber % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+        {
+            Console.WriteLine(currentNumber);
+            primeCounter++;
+        }
+        flag = 0;
+    }
+    Console.WriteLine($"Total prime numbers: {primeCounter}");
+}
